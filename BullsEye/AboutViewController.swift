a7519ebe008213749320d881087f8242c250a6c7
633squadron <633squadron@gmail.com>
2015-09-07 17:32:29 -0700

    Added resources

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Joe Malin on 2015-09-07.
+//  Copyright (c) 2015 The Arwed Group. All rights reserved.
+//
+
+import UIKit
+class AboutViewController: UIViewController {
+    
+    @IBAction func close() {
+    
+    dismissViewControllerAnimated(true, completion: nil)
+    
+    }
+}
--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let middleSliderValue: Int = 50
     var currentSliderValue: Int = 0
     var targetValue: Int = 0
-    var score: Int = 0
-    var round: Int = 0
+    var total_points: Int = 0
+    var current_round: Int = 0
     
     // Outlet from the slider widget
     @IBOutlet weak var slider: UISlider!
@@ -21,12 +22,9 @@ class ViewController: UIViewController {
     // Outlet to the displayed value of the target
     @IBOutlet weak var targetLabel: UILabel!
     
-    // Outlet to the displayed score
-    @IBOutlet weak var scoreLabel: UILabel!
-    
-    // Outlet to the current slider value
-    @IBOutlet weak var sliderLabel: UILabel!
-    
+    // Outlet to the displayed total_points
+    @IBOutlet weak var pointsLabel: UILabel!
+        
     // Outlet to the current round value
     @IBOutlet weak var roundLabel: UILabel!
     
@@ -35,8 +33,10 @@ class ViewController: UIViewController {
         
         // Do any additional setup after loading the view, typically from a nib.
         
+        current_round = 1
+        
         // Start a new round
-        startNewRound()
+        newPlay()
         
         // Update the values in the UI
         updateLabels()
@@ -48,69 +48,109 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    // Start a new round
-    
-    func startNewRound() {
-        
-        // reset the random target
-        targetValue = 1 + Int(arc4random_uniform(100))
-        
-        // Set the slider value to 50
-        currentSliderValue = 50
-        
-        // Set the slider widget position back to 50
-        slider.value = Float(currentSliderValue)
-
-        // Reset the score
-        score = 0
-        
-        // Increment the round
-        round++
-        
-    }
-    
-    func updateLabels() {
-        targetLabel.text = String(targetValue)
-        scoreLabel.text = String(score)
-        sliderLabel.text = String(currentSliderValue)
-    }
-    
-    func calcDifference(sliderVal:Int, _ targetVal:Int) -> Int {
-        // Calculate the difference between the target value and the slider value
-    
-        return abs(sliderVal - targetVal)
-    }
-
     @IBAction func showAlert() {
         var diff: Int;
         var message: String;
+        var title: String;
         
-        let points = 100 - calcDifference(currentSliderValue, targetValue)
+        var score = 100 - calcDifference(currentSliderValue, targetValue)
+ 
+        title = calcTitle(score)
         
+        // Add a bonus for a perfect score
+        if (score == 100) {
+            
+            score += 100
+            
+        }
 
         // update the score for the round
-        score += points
+        total_points += score
         
-        message = "You scored: \(points)"
+        message = "You scored: \(score)"
         
-        let alert = UIAlertController(title: "Result",
+        
+        
+        let alert = UIAlertController(title: title,
             message: message, preferredStyle: .Alert)
         
-        let action = UIAlertAction(title: "OK", style: .Default,
-            handler: nil)
+        let action = UIAlertAction(title: "OK",
+            style: .Default,
+            handler: {action in
+                self.newPlay()
+                self.updateLabels()})
         
         alert.addAction(action)
         presentViewController(alert, animated: true, completion: nil)
         
-        // Start a new round
-        startNewRound()
-        
-        // Update the values in the UI
-        updateLabels()
     }
+    
     @IBAction func sliderMoved(slider: UISlider) {
         
         currentSliderValue = lroundf(slider.value)
+    }
+    
+    func newPlay() {
+        
+        // reset the random target
+        targetValue = 1 + Int(arc4random_uniform(100))
+        
+        // Set the slider widget position back to the default value
+        currentSliderValue = middleSliderValue
+        
+        slider.value = Float(currentSliderValue)
+  
+    }
+    
+    @IBAction func newRound() {
+        
+        // Increment the round
+        current_round++
+        
+        // Reset the play total
+        total_points = 0
+
+        newPlay()
+        
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        pointsLabel.text = String(total_points)
+        roundLabel.text = String(current_round)
+    }
+    
+    func calcDifference(sliderVal:Int, _ targetVal:Int) -> Int {
+        // Calculate the difference between the target value and the slider value
+        
+        return abs(sliderVal - targetVal)
+    }
+    
+    func calcTitle(score:Int) -> String {
+        var title: String = ""
+        
+        if (score < 25) {
+            
+            return "You're a LOSER!"
+            
+        } else if ((score >= 25) && (score < 70)) {
+            
+            return "Oh, come ON!"
+            
+        } else if ((score >= 70) && (score < 90)) {
+            
+            return "Meh!"
+            
+        } else if ((score >= 90) && (score < 100)) {
+            
+            return "You almost had it!"
+        
+        } else {
+            
+            return "Perfect!"
+            
+        }
     }
 }
 
